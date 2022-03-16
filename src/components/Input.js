cc5a05f078ac20cbefa03074c1ca31246dbd70b9
Morghot2 <maximilian.jaron@gmail.com>
2022-03-16 12:48:24 +0100

    cleanup

--- src/components/Input.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
-const PostCrypto = () => {
-  const state = useSelector((state) => state.crypto);
-  return <div>CryptoElement</div>;
-};
-
-export default PostCrypto;
