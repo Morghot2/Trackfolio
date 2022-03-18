5c8530e3be167d476648f6da846857b8733155cc
Morghot2 <maximilian.jaron@gmail.com>
2022-03-18 17:06:11 +0100

    replacing semantic ui with own css

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import DisplayCoin from "./DisplayCoin";
+import '../style.css'
+
 
 const App = () => {
+  const refs = useRef([]);
   const [coin, setCoin] = useState("");
   const [coinsList, setCoinsList] = useState([]);
   const onInputChange = (event) => {
     event.preventDefault();
     setCoin(event.target.value);
   };
-  // const displaySum = () => {
-  //   const sum = document.getElementsByName("amount");
-  //   let displayedValue = 0;
-  //   for (let element of sum) {
-  //     displayedValue += parseInt(element.value);
-  //   }
-  // };
-  // const totalCalc = (coinsList) => {
-  //   const initialValue = 0
-  //   for (const crypto of coinsList) {
-  //     document.getElementById(`${coin.name}`).value * coin.market_data.current_price.usd
-  //   }
-  // }
+  const calculateTotal = () => {
+    let start = 0
+    for (let i = 0; i < refs.current.length; i++) {
+      start += parseInt(refs.current[i].innerHTML) 
+      console.log(start)
+      console.log(typeof parseInt(refs.current[i].innerHTML))
+      console.log(refs)
+    }
+  }
+  
 
   const fetchCrypto = () => {
     const btn = document.getElementById("fetchButton");
@@ -33,7 +33,6 @@ const App = () => {
         console.log("Button Activated");
       }, 300);
     };
-
     const url = `https://api.coingecko.com/api/v3/coins/${coin}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
     if (coinsList.some((element) => element.id === coin)) {
       return null;
@@ -52,16 +51,6 @@ const App = () => {
       coinsList.filter((element) => coinPosition !== coinsList.indexOf(element))
     );
   };
-
-  // const valueDivs = document.getElementsByClassName("ui segment value");
-  // const calculateTotal = (valueDivs) => {
-  //   for (let elementDiv of valueDivs) {
-  //     console.log("dupa");
-  //   }
-  // };
-  // calculateTotal();
-
-  // console.log(valueDivs);
 
   return (
     <div>
@@ -83,10 +72,10 @@ const App = () => {
           </button>
         </div>
       </div>
-      <DisplayCoin coinsList={coinsList} removeCrypto={removeCrypto} />
+      <DisplayCoin coinsList={coinsList} removeCrypto={removeCrypto} calculateTotal={calculateTotal}/>
       <div>
         Your total assets value:
-        {/*displaySum()*/}
+        <button onClick={calculateTotal()}></button>
       </div>
     </div>
   );
--- src/components/DisplayCoin.js
+++ src/components/DisplayCoin.js
@@ -7,65 +7,57 @@ const DisplayCoin = ({ coinsList, removeCrypto }) => {
     event.preventDefault();
     setCoins(event.target.value);
   };
-  let start = 0
-  for (let i = 0; i < refs.current.length; i++) {
-    //refs.current[i].innerHTML += start
-    console.log(typeof parseInt(refs.current[i].innerHTML))
-  }
-  // const currentRefs = refs.current
-  // const totalValue = (currentRefs) => {
-  //   for (let ref of currentRefs) {
-  //     console.log(ref);
-  //   }
-  // };
-  // totalValue();
-  return coinsList.map((coin) => {
-    return (
-      <div className="ui equal width grid" key={coin.name}>
-        <div className="column">
-          <div className="ui segment">
-            {coinsList.indexOf(coin) + 1}.
-            <img src={`${coin.image.thumb}`} alt="symbol"></img>
-            {coin.name}
+
+
+  return (
+    coinsList.map((coin) => {
+      return (
+        <div className="ui equal width grid" key={coin.name}>
+          <div className="column">
+            <div className="ui segment">
+              {coinsList.indexOf(coin) + 1}.
+              <img src={`${coin.image.thumb}`} alt="symbol"></img>
+              {coin.name}
+            </div>
           </div>
-        </div>
-        <div className="column">
-          <div className="ui segment">{coin.market_data.current_price.usd}</div>
-        </div>
-        <div className="column">
-          <div className="ui segment">
-            <input
-              type="number"
-              name="amount"
-              onChange={onCoinEnter}
-              /*value={coinValue}*/ id={coin.name}
-            />
+          <div className="column">
+            <div className="ui segment">{coin.market_data.current_price.usd}</div>
           </div>
-        </div>
-        <div className="column">
-          <div
-            className="ui segment value"
-            ref={(element) => {
-              refs.current[coinsList.indexOf(coin)] = element;
+          <div className="column">
+            <div className="ui segment">
+              <input
+                type="number"
+                name="amount"
+                onChange={onCoinEnter}
+                /*value={coinValue}*/ id={coin.name}
+              />
+            </div>
+          </div>
+          <div className="column">
+            <div
+              className="ui segment value"
+              ref={(element) => {
+                refs.current[coinsList.indexOf(coin)] = element;
+              }}
+            >
+              {document.getElementById(`${coin.name}`)
+                ? document.getElementById(`${coin.name}`).value *
+                  coin.market_data.current_price.usd
+                : 0}
+            </div>
+          </div>
+          <button
+            onClick={() => {
+              removeCrypto(coin);
             }}
+            className="ui button negative"
           >
-            {document.getElementById(`${coin.name}`)
-              ? document.getElementById(`${coin.name}`).value *
-                coin.market_data.current_price.usd
-              : 0}
-          </div>
+            Delete
+          </button>
         </div>
-        <button
-          onClick={() => {
-            removeCrypto(coin);
-          }}
-          className="ui button negative"
-        >
-          Delete
-        </button>
-      </div>
-    );
-  });
+      );
+    })
+  )
 };
 
 export default DisplayCoin;
