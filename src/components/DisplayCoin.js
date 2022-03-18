1e4b8ab9857a268701a33d368d2816cf44c6714e
Morghot2 <maximilian.jaron@gmail.com>
2022-03-18 17:23:21 +0100

    first steps

--- src/components/DisplayCoin.js
+++ src/components/DisplayCoin.js
@@ -8,56 +8,49 @@ const DisplayCoin = ({ coinsList, removeCrypto }) => {
     setCoins(event.target.value);
   };
 
-
-  return (
-    coinsList.map((coin) => {
-      return (
-        <div className="coin-container" key={coin.name}>
-          <div className="column">
-            <div className="ui segment">
-              {coinsList.indexOf(coin) + 1}.
-              <img src={`${coin.image.thumb}`} alt="symbol"></img>
-              {coin.name}
-            </div>
-          </div>
-          <div className="column">
-            <div className="ui segment">{coin.market_data.current_price.usd}</div>
-          </div>
-          <div className="column">
-            <div className="ui segment">
-              <input
-                type="number"
-                name="amount"
-                onChange={onCoinEnter}
-                /*value={coinValue}*/ id={coin.name}
-              />
-            </div>
-          </div>
-          <div className="column">
-            <div
-              className="ui segment value"
-              ref={(element) => {
-                refs.current[coinsList.indexOf(coin)] = element;
-              }}
-            >
-              {document.getElementById(`${coin.name}`)
-                ? document.getElementById(`${coin.name}`).value *
-                  coin.market_data.current_price.usd
-                : 0}
-            </div>
-          </div>
-          <button
-            onClick={() => {
-              removeCrypto(coin);
-            }}
-            className="ui button negative"
-          >
-            Delete
-          </button>
+  return coinsList.map((coin) => {
+    return (
+      <div className="coin-container" key={coin.name}>
+        <div className="segment">
+          {coinsList.indexOf(coin) + 1}.
+          <img src={`${coin.image.thumb}`} alt="symbol"></img>
+          {coin.name}
         </div>
-      );
-    })
-  )
+
+        <div className="segment">{coin.market_data.current_price.usd}</div>
+
+        <div className="segment">
+          <input
+            type="number"
+            name="amount"
+            onChange={onCoinEnter}
+            /*value={coinValue}*/ id={coin.name}
+          />
+        </div>
+
+        <div
+          className="segment"
+          ref={(element) => {
+            refs.current[coinsList.indexOf(coin)] = element;
+          }}
+        >
+          {document.getElementById(`${coin.name}`)
+            ? document.getElementById(`${coin.name}`).value *
+              coin.market_data.current_price.usd
+            : 0}
+        </div>
+
+        <button
+          onClick={() => {
+            removeCrypto(coin);
+          }}
+          className="delete-button"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
 };
 
 export default DisplayCoin;
