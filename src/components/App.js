b59c49ad41b117aac1af6eea4284cc44692d8bdc
Morghot2 <maximilian.jaron@gmail.com>
2022-03-29 19:55:59 +0200

    Finally working

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,45 @@ import "../style.css";
 import search from "../search.png";
 
 const App = () => {
-  // let [total, setTotal] = useState(0);
+  let [total, setTotal] = useState(0);
+  // const [coins, setCoins] = useState([]);
   const refs = useRef([]);
   const [coin, setCoin] = useState("");
   const [coinsList, setCoinsList] = useState([]);
+  const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
   const onInputChange = (event) => {
     event.preventDefault();
     setCoin(event.target.value);
   };
   // const calculateTotal = () => {
+  //   const results = refs.current.filter(element => {
+  //     return element !== null;
+  //   });
   //   let sum = 0;
   //   for (let i = 0; i < refs.current.length; i++) {
-  //     sum += parseInt(refs.current[i].innerHTML);
+  //     refs.current[i].innerHTML !== null ? sum += parseInt(refs.current[i].innerHTML) : console.log('dupa')
   //   }
   //   setTotal(sum);
   // };
+
+  const calculateTotal = () => {
+    const results = refs.current.filter(element => {
+      return element !== null;
+    });
+    let sum = 0;
+    for (let i = 0; i < results.length; i++) {
+      sum += parseInt(results[i].innerHTML) 
+    }
+    setTotal(sum);
+  };
+  // calculateTotal()
+  useEffect(() => {})
+
+
+
+
+
   const fetchCrypto = async () => {
     const url = `https://api.coingecko.com/api/v3/coins/${coin.toLowerCase()}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
     try {
@@ -40,10 +63,24 @@ const App = () => {
   };
   const removeCrypto = (coin) => {
     const coinPosition = coinsList.indexOf(coin);
+    setCoins(0)
+    
+    // refs.current.splice(coinsList.indexOf(coin), 1, 'dupa')
     setCoinsList(
       coinsList.filter((element) => coinPosition !== coinsList.indexOf(element))
     );
   };
+  // const onValueEnter = (event, coinIndex) => {
+  //   event.preventDefault();
+  //   // coins.splice(coinsList.indexOf(coin), 1, event.target.value);
+  //   coins.push(event.target.value)
+
+  //   // setCoins(event.target.value)
+    
+  //   // calculateTotal()
+  //   // setCoins(event.target.value)
+    
+  // };
 
   return (
     <div className="coin-app">
@@ -81,11 +118,16 @@ const App = () => {
         coinsList={coinsList}
         removeCrypto={removeCrypto}
         refs={refs}
-        // calculateTotal={calculateTotal}
+        calculateTotal={calculateTotal}
+        coins={coins}
+        setCoins={setCoins}
         // total={total}
+        // onValueEnter={onValueEnter}
+        // coins={coins}
       />
-      {/* <div>Your total assets value: ${total}</div> */}
-      {/* <button onClick={calculateTotal}>Total</button> */}
+      
+      <button onClick={calculateTotal}>Total</button>
+      <div>Your total assets value: ${total}</div>
     </div>
   );
 };
